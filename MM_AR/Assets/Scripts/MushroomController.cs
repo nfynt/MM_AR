5406899a1d74f106bfd88a9738c20e3984b890a4
Shubham Singh <shubham.nfynt@gmail.com>
2018-09-26 23:41:31 +0530

    Added anim controller for mushroom mon prefab

---
+++ MM_AR/Assets/Scripts/MushroomController.cs
@@ -0,0 +1,67 @@
+﻿using MMAR;
+using UnityEngine;
+using System.Collections;
+
+using DLog = MMAR.LogManager;
+
+[RequireComponent(typeof(Animator))]
+public class MushroomController : MonoBehaviour {
+
+    public delegate void EnemyDied();
+    public event EnemyDied died;
+
+    public float finalScale = 5f;
+
+    private Animator animator;
+    private bool isDead = false;
+    private float startTime;
+    private Vector3 growthRate;
+    private float timeLeft = 25f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        startTime = Time.time;
+        growthRate = Vector3.one * (Time.deltaTime/(finalScale-transform.localScale.x));
+    }
+
+    private void Update()
+    {
+        if (isDead)
+            return;
+
+        if (Time.time - startTime < timeLeft)
+            transform.localScale += growthRate;
+
+    }
+
+    public void TogglePauseState(bool pause = false)
+    {
+        if(pause)
+        {
+            timeLeft -= Time.time - startTime;
+        }
+        else
+        {
+            startTime = Time.time;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (isDead)
+            return;
+        DLog.Log("Clicked");
+        isDead = true;
+        StartCoroutine(PlayDeadAnim());
+    }
+
+    IEnumerator PlayDeadAnim()
+    {
+        animator.SetBool("Die", true);
+        if (died != null)
+            died.Invoke();
+        yield return new WaitForSeconds(1.2f);
+        Destroy(this.gameObject);
+    }
+}
